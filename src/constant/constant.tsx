6bf0629796ee62e1fa89da6078cc62fc2da6e633
Kr_Himanshu <155032817+KrHimanshu18@users.noreply.github.com>
2025-03-15 13:16:22 +0530

    constants updated
    snippets changed to comments

--- src/constant/constant.tsx
+++ src/constant/constant.tsx
@@ -7,15 +7,15 @@ export const LANGUAGE_MAP = {
 
 export const CODE_SNIPPETS = {
   javascript: {
-    helloWorld: "Write your code here",
+    helloWorld: "//Write your code here",
   },
   python: {
-    helloWorld: "Write your code here",
+    helloWorld: "#Write your code here",
   },
   java: {
-    helloWorld: "Write your code here",
+    helloWorld: "//Write your code here",
   },
   cpp: {
-    helloWorld: "Write your code here",
+    helloWorld: "//Write your code here",
   },
 };
